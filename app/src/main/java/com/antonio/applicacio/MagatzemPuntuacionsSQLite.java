5dd33c74515f2d74bd05c44b16425e64c3fb177d
Antonio Ortiz <totti619@me.com>
2018-02-07 16:39:55 +0100

    App33

--- app/src/main/java/com/antonio/applicacio/MagatzemPuntuacionsSQLite.java
+++ app/src/main/java/com/antonio/applicacio/MagatzemPuntuacionsSQLite.java
@@ -2,8 +2,10 @@ package com.antonio.applicacio;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.util.Vector;
 
@@ -24,8 +26,18 @@ public class MagatzemPuntuacionsSQLite extends SQLiteOpenHelper implements Magat
         // Aqui s'han de crear totes les taules de la BD, i
         // inicialitzar les dades si es necessari.
         // CREATE TABLE nom_taula(nom_columna tipus [atributs], ... )
-        db.execSQL("CREATE TABLE vpuntuacions (" + "_id INTEGER PRIMARY "+
-            "KEY AUTOINCREMENT," + "punts INTEGER, nom TEXT, data LONG)");
+        db.execSQL("CREATE TABLE usuari (" +
+                    "usu_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "nom TEXT UNIQUE," +
+                    "correu TEXT" +
+                ")");
+        db.execSQL("CREATE TABLE puntuacio (" +
+                    "punts_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "punts INTEGER," +
+                    "data TEXT," +
+                    "usuari INTEGER, " +
+                    "FOREIGN KEY (usuari) REFERENCES usuari(usu_id)" +
+                ")");
     }
 
     @Override
@@ -40,9 +52,24 @@ public class MagatzemPuntuacionsSQLite extends SQLiteOpenHelper implements Magat
         // Sentencia SQL que afegeix una fila a la taula.
         // INSERT INTO nom_taula VALUES (valor1,valor2, ...)
         // Els valors cadena can entre cometes
-        db.execSQL("INSERT INTO vpuntuacions VALUES (null, "+punts+","+
-            "'"+nom+"', "+data+")");
-        db.close();
+        try{
+            db.execSQL("INSERT INTO usuari VALUES(null,'"+nom+"'," +
+                    "'"+nom.toLowerCase().charAt(0)+"@upv.es')");
+        }catch (SQLException e){
+            Log.e("SQLException: ",e.getMessage());
+        }finally {
+            Cursor cursor=db.rawQuery(
+                    "SELECT usu_id FROM usuari WHERE usuari.nom='"+nom+"'"
+                    ,null
+            );
+            cursor.moveToFirst();
+            db.execSQL("INSERT INTO puntuacio VALUES (null, "+punts+","+
+                    "'"+data+"', "+cursor.getInt(0)+")");
+            cursor.close();
+            db.close();
+            Log.d("hasta aqui:","hasta aqui");
+        }
+
     }
 
     @Override
@@ -53,10 +80,10 @@ public class MagatzemPuntuacionsSQLite extends SQLiteOpenHelper implements Magat
         // Obte un cursor que conte totes les files de la consulta.
         // SELECT columna1,... FROM nom_taula ORDER BY columna [mode]
         // LIMIT numero
-        Cursor cursor=db.rawQuery("SELECT punts, nom FROM "+
-            "vpuntuacions ORDER BY punts DESC LIMIT "+quantitat,null);
+        Cursor cursor=db.rawQuery(
+                "SELECT puntuacio.punts, usuari.nom, puntuacio.data FROM puntuacio JOIN usuari ON usuari.usu_id=puntuacio.punts_id ORDER BY puntuacio.data DESC LIMIT "+quantitat,null);
         while(cursor.moveToNext()) {
-            result.add(cursor.getInt(0)+" "+cursor.getString(1));
+            result.add(cursor.getInt(0)+" "+cursor.getString(1)+" "+cursor.getString(2));
         }
         cursor.close();
         db.close();
